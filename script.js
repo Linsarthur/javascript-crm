7aa423fa9b18fe633934ca40ff3b41472ce5bb57
Arthur Cavalcante <linsarthur02@gmail.com>
2025-05-04 19:37:06 -0300

    atualização e aplicação da api mockada

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+fetch("http://localhost:3000/users").then(res => res.json()).then(data => {
+    const tbody = document.querySelector("#user-table-body");
+    data.forEach(user => {
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <tr class="flex justify-between">
+            <td class="px-4 py-2">${user.id}</td>
+            <td class="px-4 py-2">${user.name}</td>
+            <td class="px-4 py-2">${user.email}</td>
+            <td class="px-4 py-2">${user.status}</td>
+        </tr>
+        `;
+
+        tbody.appendChild(row)
+    });
+})
+
+
 function mostrarOverlay() {
     let overlayElement = document.querySelector("#overlay")
     let gavetaElement = document.querySelector("#gaveta")
@@ -6,13 +25,23 @@ function mostrarOverlay() {
         overlayElement.classList.remove("invisible", "opacity-0")
         gavetaElement.classList.remove("-right-full")
         gavetaElement.classList.add("right-0")
-    
+
     } else {
         overlayElement.classList.add("invisible", "opacity-0")
         gavetaElement.classList.remove("right-0")
         gavetaElement.classList.add("-right-full")
     }
+}
 
+function voltarHome() {
+    window.location.href = "/"
+}
 
+function mudarParaClientes() {
+    let captureIdClientes = document.querySelector("#clientes")
+    window.location.href = "/clientes.html"
+}
 
+function mudarParaProdutos() {
+    window.location.href = "/produtos.html"
 }
