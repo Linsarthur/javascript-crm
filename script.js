8b96593eae55732577130e0eab3937658a065755
arthur cavalcante <linsarthur02@gmail.com>
2025-05-05 16:47:50 -0300

    alterações feitas em aula

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-fetch("http://localhost:3000/users").then(res => res.json()).then(data => {
-    const tbody = document.querySelector("#user-table-body");
-    data.forEach(user => {
-        const row = document.createElement('tr');
+// fetch("http://localhost:3000/users").then(res => res.json()).then(data => {
+//     const tbody = document.querySelector("#user-table-body");
+//     data.forEach(user => {
+//         const row = document.createElement('tr');
 
-        row.innerHTML = `
+//         row.innerHTML = `
         
-            <td>${user.id}</td>
-            <td>${user.name}</td>
-            <td>${user.email}</td>
-            <td>${user.status}</td>
+           
+//             <td>${user.name}</td>
+//             <td>${user.email}</td>
+//             <td>${user.status}</td>
         
-        `;
+//         `;
 
-        tbody.appendChild(row)
-    });
-})
+//         tbody.appendChild(row)
+//     });
+// })
 
 
 function mostrarOverlay() {
